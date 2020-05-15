b7a3c37c706366be698f515c5ff71ce346977a2c
Kosuke Watanabe <k19981202m4@gmail.com>
2020-05-16 01:28:24 +0900

    change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function TodoItem(props){
+  return(
+  <li>
+    <input type = "checkbox"
+      checked = {props.todo.isDone}
+      onChange = {()=>{props.checkTodo(props.todo)}}
+   />
+    {props.todo.title}
+    <span onClick = {()=>{props.deleteTodo(props.todo)}}> [×]</span>
+  </li>
+  )
+}
+
+function Todolist(props){
+  const todos = props.todos.map(todo => {
+    return(
+      <TodoItem
+       key = {todo.id}
+       todo = {todo}
+       checkTodo = {props.checkTodo}
+       deleteTodo = {props.deleteTodo}
+      />
+    )
+  })
+
+  return(
+    <ul>
+      {todos}
+    </ul>
   );
 }
 
+
+const todos = [
+  { id:1, title: "title A", isDone: true },
+  { id:2, title: "title B", isDone: true },
+  { id:3, title: "title C", isDone: false }
+]
+
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: todos
+    }
+    this.checkTodo = this.checkTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+  }
+
+  checkTodo(todo){
+    const todos = this.state.todos.map(todo =>{
+      return{ id: todo.id, title: todo.title, isDone: todo.isDone}
+    })
+
+    const pos = this.state.todos.map(todo =>{
+      return todo.id;
+    }).indexOf(todo.id)
+
+    todos[pos].isDone = !todos[pos].isDone;
+    this.setState({
+      todos: todos
+    })
+  }
+
+  deleteTodo(todo){
+    if (!window.confirm("削除しますか？")){
+      return;
+    }
+
+    const todos = this.state.todos.map(todo =>{
+      return{ id: todo.id, title: todo.title, isDone: todo.isDone}
+    })
+
+   const pos = this.state.todos.indexOf(todo)
+   todos.splice(pos, 1)
+   this.setState({
+     todos: todos
+   })
+
+
+  }
+
+
+  render(){
+    return (
+      <div className="container">
+        <p>Tdo list </p>
+        <ul>
+         <Todolist
+          todos = {this.state.todos}
+          checkTodo = {this.checkTodo}
+          deleteTodo = {this.deleteTodo}
+         />
+        </ul>
+      </div>
+    );
+  }
+}
+
 export default App;
+
+
